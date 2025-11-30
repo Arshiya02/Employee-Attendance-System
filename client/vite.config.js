139fe95aa0db5a46bd9276cacac9622f4dea62cf
Arshiya02 <arshiyaanjumshaik5@gmail.com>
2025-11-30 12:45:22 +0530

    Switch to CommonJS module syntax in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+const { defineConfig } = require('vite');
+const react = require('@vitejs/plugin-react').default;
 
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
   },
-})
+});
